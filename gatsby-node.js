70121f31ffe834bd0ea05eaf7007666ef5a38e64
lukemiller93 <lukem1293@gmail.com>
2019-03-26 14:08:19 -0500

    added tags for products

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,8 @@ exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
   const productTemplate = path.resolve(`src/templates/productTemplate.js`)
+  const tagPage = path.resolve(`src/pages/specials.js`)
+  const tagPosts = path.resolve(`src/templates/tag.js`)
 
   return graphql(`
     {
@@ -20,6 +22,8 @@ exports.createPages = ({ actions, graphql }) => {
           node {
             id
             frontmatter {
+              size
+              style
               serial
             }
           }
@@ -31,12 +35,56 @@ exports.createPages = ({ actions, graphql }) => {
       return Promise.reject(result.errors)
     }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const products = result.data.allMarkdownRemark.edges
+
+    //create tags page
+    const productsByTag = {}
+
+    products.forEach(({ node }) => {
+      if (node.frontmatter.size) {
+        if (!productsByTag[node.frontmatter.size]) {
+          productsByTag[node.frontmatter.size] = []
+        }
+        productsByTag[node.frontmatter.size].push(node)
+      }
+    })
+
+    const tags = Object.keys(productsByTag)
+
+    createPage({
+      path: "/specials",
+      component: tagPage,
+      context: {
+        tags: tags.sort(),
+      },
+    })
+
+    // create tags
+    tags.forEach(tagName => {
+      const taggedProducts = productsByTag[tagName]
+
+      createPage({
+        path: `/tags/${tagName}/`,
+        component: tagPosts,
+        context: {
+          taggedProducts,
+          tagName,
+        },
+      })
+    })
+
+    // create product pages
+    products.forEach(({ node }, index) => {
+      const prev = index === 0 ? null : products[index - 1].node
+      const next =
+        index === products.length - 1 ? null : products[index + 1].node
       createPage({
         path: `/specials/${node.frontmatter.serial}/`,
         component: productTemplate,
         context: {
           id: node.id,
+          prev,
+          next,
         }, // additional data can be passed via context
       })
     })
