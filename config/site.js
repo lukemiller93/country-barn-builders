0bab7069c9e2dd3da533be79498b902c6d833ffa
lukemiller93 <lukem1293@gmail.com>
2019-04-09 16:11:34 -0500

    updated dependencies and errors

--- config/site.js
+++ config/site.js
@@ -3,7 +3,7 @@ module.exports = {
   title: `Country Barn Builders`,
   description: `Country Barn Builders is a full service portable storage shed company that specializes in standard and custom solutions for any budget. We service London, KY and the surrounding areas.`,
   url: "https://practical-mestorf-9c8a21.netlify.com",
-  siteURL: "https://practical-mestorf-9c8a21.netlify.com/",
+  siteUrl: "https://practical-mestorf-9c8a21.netlify.com/",
   siteLanguage: "en",
   logo: "/images/company-logo.png",
   banner: "/images/london-lot.jpg",
